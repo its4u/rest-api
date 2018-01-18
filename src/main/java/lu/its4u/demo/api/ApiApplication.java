d796d8c6228a4f369ec5de7303e11697ea297852
gnieser <guillaume.nieser@its4u.lu>
2018-01-18 14:31:03 +0100

    Added Keycloak authentication method

--- src/main/java/lu/its4u/demo/api/ApiApplication.java
+++
@@ -1,7 +0,0 @@
-package lu.its4u.demo.api;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ApiApplication extends Application {
-}

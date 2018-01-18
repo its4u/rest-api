4a44d967a4046997b2b76a2e4df9f0086041205b
gnieser <guillaume.nieser@its4u.lu>
2018-01-18 13:39:18 +0100

    Simple Rest API application

---
+++ src/main/java/lu/its4u/demo/api/ApiApplication.java
@@ -0,0 +1,7 @@
+package lu.its4u.demo.api;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ApiApplication extends Application {
+}
